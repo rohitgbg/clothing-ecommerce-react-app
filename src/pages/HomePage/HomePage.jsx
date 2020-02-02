2e5e54ebb861d3072a48023b55779b640e2fcb50
Rohith GB <rohit.gb@preceptlabs.com>
2020-02-02 14:57:52 +0530

    pages, components and design

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HomePage.scss";
+import Directory from "../../components/Directory/Directory";
+
+const HomePage = props => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
