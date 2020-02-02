9b80131ba2a5edc6ad18d4b8bfeeddcd04481583
Rohith GB <rohit.gb@preceptlabs.com>
2020-02-02 11:44:41 +0530

    HomePage setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <HomePage />
     </div>
   );
 }
