604c64094da69eee8cdacdf40c301f41a284a65e
Rohith GB <rohit.gb@preceptlabs.com>
2020-02-11 05:23:02 +0530

    cart items in cart dropdown

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import "./CartDropdown.scss";
 
 import CustomButton from "../CustomButton/CustomButton";
+import { connect } from "react-redux";
+import CartItem from "../CartItem/CartItem";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems: cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
