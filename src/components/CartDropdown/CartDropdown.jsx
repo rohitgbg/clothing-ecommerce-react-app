f5b993a0e98b7e08c354623d587fd941510fc75a
Rohith GB <rohit.gb@preceptlabs.com>
2020-02-11 06:21:50 +0530

    implemented reselector package to achieve memoization - cartitems and cartitemsCount

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -4,20 +4,23 @@ import "./CartDropdown.scss";
 import CustomButton from "../CustomButton/CustomButton";
 import { connect } from "react-redux";
 import CartItem from "../CartItem/CartItem";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+      {cartItems.length > 0
+        ? cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        : "Add items to cart"}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems: cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
